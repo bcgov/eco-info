b003ff854c06c513fbe7fee606945e16a7f0509a
Bauger-BC <70655859+Bauger-BC@users.noreply.github.com>
2021-02-22 13:43:20 -0800

    Add files via upload
    Demo script for accessing the geometry of a grid feature.

---
+++ scripts/utilities/Geometry demo.py
@@ -0,0 +1,23 @@
+import arcpy
+
+infc = 'BC_Grids_All_20k_to_BC'
+
+# If map sheet count >1 use dissolve sso its pretty for Deepa, if 1 sheet copy geometry
+
+
+# Pass a feature layer created in mememory into the search cursor, it will grab geometry from those records, put into an array
+# Features passed into array are a series of points
+array = arcpy.Array()
+
+for row in arcpy.da.SearchCursor(infc, ["SHAPE@"]):
+    row[0].generalize(.1)
+    for point in row[0]:
+        for pts in point:
+            array.add(pts)
+
+# Convert array into polygon
+poly = arcpy.Polygon(array)
+
+# Insert polygon into destination feature along wtih all the other attributes from excel file
+cursor = arcpy.da.InsertCursor('test', ['SHAPE@'])
+cursor.insertRow([poly.convexHull()])
